d35150861464ec9b2c926d1f4fea06e1b160f97e
maximilien0405 <maximilien.zimmermann@ik.me>
2023-01-31 15:14:29 +0100

    feat(app): changes on structure + adding favicon

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: "http://localhost:8005",
+  jwtToken: "32e7a480a9624193a317edb2c2d25d0cf4de2cd8fe564e66b85209847ab7a8a3b3620bd2500b48468e2911926225b07634423053c4224e348fcec68939546d54"
 };
 
 /*
